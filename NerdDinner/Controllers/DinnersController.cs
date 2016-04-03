a327b07acd4f6f73d42660b93c8d691b81dbdc67
Mayur23491 <padhiyar2341991@gmail.com>
2016-04-03 22:10:49 +0530

    Firsttime upload

---
+++ NerdDinner/Controllers/DinnersController.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using NerdDinner.Models;
+
+namespace NerdDinner.Controllers
+{
+    public class DinnersController : Controller
+    {
+        //
+        // GET: /Dinners/
+        DinnerRepository repository = new DinnerRepository();
+        public ActionResult Index()
+        {
+            var dinners = repository.FindUpcomingDinners().ToList();
+            return View("Index", dinners);
+        }
+
+        public ActionResult Details(int id)
+        {
+            Dinner dinner = repository.GetDinner(id);
+
+            if (dinner == null)
+                return View("NotFound");
+            else
+                return View("Details", dinner);
+        }
+
+        public ActionResult Create()
+        {
+            Dinner dinner = new Dinner()
+            {
+                EventDate = DateTime.Now.AddDays(7)
+            };
+            return View(dinner);
+        }
+
+        [HttpPost]
+        public ActionResult Create(FormCollection formValues) 
+        {
+            Dinner dinner = new Dinner();
+            if(TryUpdateModel(dinner)) 
+            {
+            repository.Add(dinner);
+            repository.Save();
+            return RedirectToAction("Details", new {id=dinner.DinnerID});
+            }
+            return View(dinner);
+        }
+
+        public ActionResult Edit(int id)
+        {
+            Dinner dinner = repository.GetDinner(id);
+            return View(dinner);
+        }
+
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection formValues)
+        {
+            Dinner dinner = repository.GetDinner(id);
+            UpdateModel(dinner);
+            if (TryUpdateModel(dinner))
+            {
+                repository.Save();
+                return RedirectToAction("Details", new { id = dinner.DinnerID });
+            }
+            return View(dinner);
+        }
+
+        public ActionResult Delete(int id) 
+        {
+            Dinner dinner = repository.GetDinner(id);
+            if (dinner == null)
+                return View("NotFound");
+            else
+                return View(dinner);
+        }
+
+        [HttpPost]
+        public ActionResult Delete(int id, string confirmButton) 
+        {
+            Dinner dinner = repository.GetDinner(id);
+            if (dinner == null)
+                return View("NotFound");
+            repository.Delete(dinner);
+            repository.Save();
+            return View("Deleted");
+        }
+    }
+}
---
+++ NerdDinner/Models/DinnerRepository.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace NerdDinner.Models
+{
+    public class DinnerRepository
+    {
+        private NerdDinnerEntities entities = new NerdDinnerEntities();
+
+        public IQueryable<Dinner> FindAllDinners()
+        {
+            return entities.Dinners;
+        }
+
+        public IQueryable<Dinner> FindUpcomingDinners()
+        {
+            return from dinner in entities.Dinners
+                   where dinner.EventDate > DateTime.Now
+                   orderby dinner.EventDate
+                   select dinner;
+        }
+
+        public Dinner GetDinner(int id)
+        {
+            return entities.Dinners.FirstOrDefault`(d => d.DinnerID == id);
+        }
+
+        public void Add(Dinner dinner)
+        {
+            entities.Dinners.Add(dinner);
+        }
+
+        public void Delete(Dinner dinner)
+        {
+            entities.Dinners.Remove(dinner);
+        }
+
+        public void Save()
+        {
+            entities.SaveChanges();
+        }
+    }
+}
